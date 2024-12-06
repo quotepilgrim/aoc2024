b122e872f156259cca134b19b55d14e4f0651f71
Quote Pilgrim <caio.psv@gmail.com>
2024-12-06 02:02:30 -0300

    DAY 5 DONZO!

--- day5/main.lua
+++ day5/main.lua
@@ -26,38 +26,79 @@ local function get_data(input)
     return rules, updates
 end
 
+local function is_greater(a, b, rules)
+    local a_pos, b_pos
+    for _, rule in ipairs(rules) do
+        a_pos = rule:find(a)
+        b_pos = rule:find(b)
+        if a_pos and b_pos then
+            return a_pos > b_pos
+        end
+    end
+    return nil
+end
+
+local function is_valid(s, rules)
+    local valid = true
+    for _, r in ipairs(rules) do
+        local a, b = r:match("(.+)|(.+)")
+        local a_pos = s:find(a)
+        local b_pos = s:find(b)
+        if a_pos and b_pos and a_pos > b_pos then
+            valid = false
+            break
+        end
+    end
+    return valid
+end
+
+local function bubblesort(t, rules)
+    local swapped
+    repeat
+        swapped = false
+        for i = 2, #t do
+            if is_greater(t[i - 1], t[i], rules) then
+                t[i - 1], t[i] = t[i], t[i - 1]
+                swapped = true
+            end
+        end
+    until not swapped
+end
+
 local function part_one()
     local rules, updates = get_data(filename)
     if rules == nil or updates == nil then
         return
     end
     for _, u in ipairs(updates) do
-        local valid = true
-        for _, r in ipairs(rules) do
-            local a, b = r:match("(.+)|(.+)")
-            local a_pos = u:find(a)
-            local b_pos = u:find(b)
-            if a_pos ~= nil and b_pos ~= nil and a_pos > b_pos then
-                valid = false
-                break
-            end
-        end
-        if valid then
-            local t ={}
+        if is_valid(u, rules) then
+            local t = {}
             for i in u:gmatch("([^,]+)") do
                 table.insert(t, i)
             end
-            local middle = t[math.ceil(#t/2)] or 0
+            local middle = t[math.ceil(#t / 2)] or 0
             answer = answer + middle
         end
     end
 end
 
 local function part_two()
-    local data = get_data(filename)
-    if data == nil then
+    local rules, updates = get_data(filename)
+    if rules == nil or updates == nil then
         return
     end
+    for _, u in ipairs(updates) do
+        if not is_valid(u, rules) then
+            local t = {}
+            for i in u:gmatch("[^,]+") do
+                table.insert(t, i)
+            end
+            bubblesort(t, rules)
+            local middle = t[math.ceil(#t / 2)] or 0
+            answer = answer + middle
+        end
+    end
+    is_greater(1, 2, rules)
 end
 
 function love.load()
