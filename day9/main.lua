3593320bebac6bd9ea9763379137a4bb70431cc4
Quote Pilgrim <caio.psv@gmail.com>
2024-12-11 00:23:14 -0300

    d9p1 done!

--- day9/main.lua
+++ day9/main.lua
@@ -11,9 +11,8 @@ local function get_data(input)
     end
 
     local line = file:read()
-    while line ~= nil do
-        table.insert(data, line)
-        line = file:read()
+    for i = 1, #line do
+        table.insert(data, line:sub(i, i))
     end
 
     file:close()
@@ -25,7 +24,48 @@ local function part_one()
     if data == nil then
         return
     end
-    print(table.concat(data, "\n"))
+    local disk = {}
+    local id = 0
+    for i = 1, #data do
+        local mod = math.fmod(i, 2)
+        for _ = 1, data[i] do
+            if mod == 1 then
+                table.insert(disk, id)
+            else
+                table.insert(disk, ".")
+            end
+        end
+        if mod == 1 then
+            id = id + 1
+        end
+    end
+    local l = 1
+    local r = #disk
+    local changed = true
+    while l < r and changed do
+        changed = false
+        if disk[r] ~= "." then
+            for i = l, r do
+                if disk[i] == "." then
+                    changed = true
+                    disk[i] = disk[r]
+                    disk[r] = "."
+                    l = i + 1
+                    break
+                end
+            end
+        else
+            changed = true
+            r = r - 1
+        end
+    end
+    for i, v in ipairs(disk) do
+        if v ~= "." then
+            answer = answer + (i - 1) * v
+        else
+            break
+        end
+    end
 end
 
 local function part_two()
